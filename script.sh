a782616acb0a5b279055758f568bfb5d11a28200
root <piero.noa@tecsup.edu.pe>
2024-09-30 21:52:38 +0000

    002

--- script.sh
+++ script.sh
@@ -2,4 +2,13 @@
 
 docker build -t blazor_image .
 
-docker run -dp 8080:80 --name blazor_container blazor_image
+CONTAINER_NAME="blazor_container"
+
+# Borrar el contenedor y capturar el resultado
+if docker rm -f "$CONTAINER_NAME"; then
+    echo "El contenedor '$CONTAINER_NAME' ha sido borrado exitosamente."
+else
+    echo "Error: No se pudo borrar el contenedor '$CONTAINER_NAME'."
+fi
+
+docker run -dp 8080:80 --name "$CONTAINER_NAME" blazor_image
